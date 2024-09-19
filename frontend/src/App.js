efed260c4c34f9264c2f8f542fea8afa8a6ca50a
Yukta Bhatia <yuktabhatia8@gmail.com>
2024-09-20 00:41:35 +0530

    Changes in layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,10 +10,13 @@ import "./App.css"; // Import custom CSS
 import Footer from "./components/Footer/Footer";
 import RainfallChart from "./components/RainfallChart/RainfallChart";
 import MapWithMarkers from "./components/MapWithMarkers.js/MapWithMarkers";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function App() {
   const [selectedRegion, setSelectedRegion] = useState({
-    name: "Birmingham",  // Default to Birmingham
+    name: "Birmingham", // Default to Birmingham
     coordinates: [52.48049047465328, -1.8978672581749725],
   });
 
@@ -25,14 +28,28 @@ function App() {
 
         {/* Page Content */}
         <div className="content">
-          <MapWithMarkers onRegionSelect={setSelectedRegion} />
-          <br />
-          <Routes>
-            {/* Redirect root path to /dashboard */}
-            <Route path="/" element={<Navigate to="/dashboard" />} />
-            <Route path="/dashboard" element={<RainfallChart selectedRegion={selectedRegion} />} />
-           
-          </Routes>
+          <Container>
+            <Row>
+              <Col sm={8} className="MapChartArea">
+                
+                <MapWithMarkers onRegionSelect={setSelectedRegion} />
+                <br />
+                <Routes>
+                  {/* Redirect root path to /dashboard */}
+                  <Route path="/" element={<Navigate to="/dashboard" />} />
+                  <Route
+                    path="/dashboard"
+                    element={<RainfallChart selectedRegion={selectedRegion} />}
+                  />
+                </Routes>
+              </Col>
+              <Col sm={1}></Col>
+              <Col className="NewsArea" sm={3}>
+              <Row></Row>
+              <Row></Row>
+              sm=4</Col>
+            </Row>
+          </Container>
         </div>
 
         {/* Footer */}
--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,24 +1,33 @@
-import React from "react"
-import {Navbar, Nav, Container} from "react-bootstrap"
-import { NavLink } from "react-router-dom"
-import './Header.css'
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import "./Header.css";
+import Rain from "./Rain.png";
 
-const Header=()=>{
-    return(
-        <Navbar bg="light" expand="xl">
-        <Container className="d-flex justify-content-start align-items-center">
-          <Navbar.Brand style={{fontSize:"26px"}}>GeoRainfall Explorer</Navbar.Brand>
-          <Nav className="ml-2">
-            <Nav.Link as={NavLink} to="/dashboard" className="nav-item" activeClassName="active">
-              Dashboard
-            </Nav.Link>
-            {/* <Nav.Link as={NavLink} to="/help" className="nav-item" activeClassName="active">
+const Header = () => {
+  return (
+    <Navbar bg="black" expand="xl">
+      <Container className="d-flex justify-content-start align-items-center">
+        <Navbar.Brand style={{ fontSize: "26px" }}>
+          <img className="RainLogo" src={Rain} />
+          {"  "}GeoRainfall Explorer
+        </Navbar.Brand>
+        <Nav className="ml-2">
+          <Nav.Link
+            as={NavLink}
+            to="/dashboard"
+            className="nav-item"
+            activeClassName="active"
+          >
+            Dashboard
+          </Nav.Link>
+          {/* <Nav.Link as={NavLink} to="/help" className="nav-item" activeClassName="active">
               Help
             </Nav.Link> */}
-          </Nav>
-        </Container>
-      </Navbar>
-    )
-}
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+};
 
 export default Header;
