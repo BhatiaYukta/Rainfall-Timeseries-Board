6ab8f8803f9b64f67c98a341e754da62c004fe7f
Yukta Bhatia <yuktabhatia8@gmail.com>
2024-09-20 08:35:14 +0530

    Added News section and tiles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,12 @@ import MapWithMarkers from "./components/MapWithMarkers.js/MapWithMarkers";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
+import Button from "react-bootstrap/Button";
+import news1 from "./news1.png";
+import news2 from "./news2.png";
+import news3 from "./news3.png";
 function App() {
   const [selectedRegion, setSelectedRegion] = useState({
     name: "Birmingham", // Default to Birmingham
@@ -31,7 +36,8 @@ function App() {
           <Container>
             <Row>
               <Col sm={8} className="MapChartArea">
-                
+                <br></br>
+
                 <MapWithMarkers onRegionSelect={setSelectedRegion} />
                 <br />
                 <Routes>
@@ -45,9 +51,42 @@ function App() {
               </Col>
               <Col sm={1}></Col>
               <Col className="NewsArea" sm={3}>
-              <Row></Row>
-              <Row></Row>
-              sm=4</Col>
+                <br></br>
+                <Card body style={{ borderColor: "white" }}>
+                  <span>
+                    <h4>
+                      <b>Top Stories</b>
+                    </h4>
+                  </span>
+                </Card>
+                <Card style={{ width: "19rem" }}>
+                  <a
+                    target="_blank"
+                    href="https://www.metoffice.gov.uk/about-us/news-and-media/media-centre/weather-and-climate-news/2024/thunderstorm-warnings"
+                  >
+                    <Card.Img variant="top" src={news1} />
+                  </a>
+                </Card>
+                <br></br>
+                <Card style={{ width: "19rem" }}>
+                  <a target="_blank" href="https://youtu.be/lrPS2HiYVp8">
+                    <Card.Img variant="top" src={news2} />
+                  </a>
+                </Card>
+                <br></br>
+                <Card style={{ width: "19rem" }}>
+                  <Card.Img variant="top" src={news3} />
+                  <Card.Body>
+                    <a
+                      target="_blank"
+                      href="https://www.youtube.com/watch?v=KWbakMx0kxA"
+                    >
+                      More heavy rain across Alabama at times Sunday. Limited
+                      ...
+                    </a>
+                  </Card.Body>
+                </Card>
+              </Col>
             </Row>
           </Container>
         </div>
